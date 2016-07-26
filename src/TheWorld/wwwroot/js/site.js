477b677de686faeb0efe58902e5a2c52912008b2
Ryan Magoon <mr.magoon5@gmail.com>
2016-07-25 21:31:23 -0400

    refactor site functionality using jquery, add alert trigger for anchor click

--- src/TheWorld/wwwroot/js/site.js
+++ src/TheWorld/wwwroot/js/site.js
@@ -1,15 +1,22 @@
 //site.js
 
-(function() {
-    var ele = document.getElementById("username");
-    ele.innerHTML = "Ryan Marshal Magoon";
+(function () {
 
-    var main = document.getElementById("main");
-    main.onmouseenter = function () {
+    var ele = $("#username");
+    ele.text("Ryan Marshal Magoon");
+
+    var main = $("#main");
+    main.on("mouseenter", function () {
         main.style = "background-color: #888";
-    };
+    });
 
-    main.onmouseleave = function () {
+    main.on("mouseleave", function () {
         main.style = "";
-    };
+    });
+
+    var menuItems = $("ul.menu li a");
+    menuItems.on("click", function () {
+        var me = $(this);
+        alert(me.text());
+    });
 })();
