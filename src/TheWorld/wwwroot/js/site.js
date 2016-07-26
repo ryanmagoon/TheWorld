c6fb3d7accb157c08171acb597bdf5d45c127cee
Ryan Magoon <mr.magoon5@gmail.com>
2016-07-26 00:57:40 -0400

    toggle and animate sidebar

--- src/TheWorld/wwwroot/js/site.js
+++ src/TheWorld/wwwroot/js/site.js
@@ -2,21 +2,32 @@
 
 (function () {
 
-    var ele = $("#username");
-    ele.text("Ryan Marshal Magoon");
+    //var ele = $("#username");
+    //ele.text("Ryan Marshal Magoon");
 
-    var main = $("#main");
-    main.on("mouseenter", function () {
-        main.style = "background-color: #888";
-    });
+    //var main = $("#main");
+    //main.on("mouseenter", function () {
+    //    main.style = "background-color: #888";
+    //});
 
-    main.on("mouseleave", function () {
-        main.style = "";
-    });
+    //main.on("mouseleave", function () {
+    //    main.style = "";
+    //});
 
-    var menuItems = $("ul.menu li a");
-    menuItems.on("click", function () {
-        var me = $(this);
-        alert(me.text());
+    //var menuItems = $("ul.menu li a");
+    //menuItems.on("click", function () {
+    //    var me = $(this);
+    //    alert(me.text());
+    //});
+
+    var $sidebarAndWrapper = $("#sidebar, #wrapper");
+
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show Sidebar");
+        } else {
+            $(this).text("Hide Sidebar");
+        }
     });
 })();
