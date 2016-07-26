4c7709677f360dd0d577d055976e496471d4b0f1
Ryan Magoon <mr.magoon5@gmail.com>
2016-07-25 21:10:13 -0400

    use IIFE to preserve limit scope

--- src/TheWorld/wwwroot/js/site.js
+++ src/TheWorld/wwwroot/js/site.js
@@ -1,4 +1,15 @@
 //site.js
 
-var ele = document.getElementById("username");
-ele.innerHTML = "Ryan Marshal Magoon";
+(function() {
+    var ele = document.getElementById("username");
+    ele.innerHTML = "Ryan Marshal Magoon";
+
+    var main = document.getElementById("main");
+    main.onmouseenter = function () {
+        main.style = "background-color: #888";
+    };
+
+    main.onmouseleave = function () {
+        main.style = "";
+    };
+})();
