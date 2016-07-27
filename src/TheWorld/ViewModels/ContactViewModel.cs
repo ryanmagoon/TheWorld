67270187d63572c5fe19ce16996027abe939de4c
Ryan Magoon <mr.magoon5@gmail.com>
2016-07-27 16:39:04 -0400

    Implement a contact page

---
+++ src/TheWorld/ViewModels/ContactViewModel.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheWorld.ViewModels
+{
+    public class ContactViewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+    }
+}
