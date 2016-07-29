462a99f15380d60f7a25d1957260cba55da55c0a
Ryan Magoon <mr.magoon5@gmail.com>
2016-07-29 19:22:13 -0400

    Design the login view

---
+++ src/TheWorld/Controllers/AuthController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheWorld.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Trips", "App");
+            }
+
+            return View();
+        }
+    }
+}
